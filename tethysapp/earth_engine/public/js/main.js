331f2dc45620d65e420e81f3c84b9a531ccfd21a
kaileymohamed <kaileymohamed@outlook.com>
2022-02-11 13:27:46 -0700

    sending dataset variable to backend

--- tethysapp/earth_engine/public/js/main.js
+++ tethysapp/earth_engine/public/js/main.js
@@ -21,35 +21,9 @@ function csrfSafeMethod(method) {
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
-function creationMaps(){
-    var vector_obj = {};
-    var baseMapsNames = [
-        'ArcGIS:DarkGray',
-        'ArcGIS:Navigation',
-        'ArcGIS:DarkGray:Base',
-        'ArcGIS:NavigationNight',
-        'ArcGIS:DarkGray:Labels',
-        'ArcGIS:Streets',
-        'ArcGIS:LightGray',
-        'ArcGIS:StreetsNight',
-        'ArcGIS:LightGray:Base',
-        'ArcGIS:StreetsRelief',
-        'ArcGIS:LightGray:Labels',
-        'ArcGIS:StreetsRelief:Base'
-    ];
-    baseMapsNames.forEach(mp=>{
-        var navigationMap = L.esri.Vector.vectorBasemapLayer(mp, {
-            apikey: "AAPK052bec1846714415aed2c85ddfa15f73KYexWiKoe0Au2nFQprFm_CWnafrYs4Y3MwTI3iqb-QBEwR808TRyXrudF4Za40V-" // Replace with your API key - https://developers.arcgis.com
-        });
-        var nameBase = mp.split(':')[1];
-        vector_obj[nameBase] = navigationMap;
-    })
-    return vector_obj
-
-}
-
 // add csrf token to appropriate ajax requests
 $(function() {
+    var input_spatial ="";
     //Start Map //
     var map = L.map('map').setView([8.913648, -79.544706], 10);
 
@@ -61,18 +35,18 @@ $(function() {
           '<a href="https://earthengine.google.com" target="_">' +
           'Google Earth Engine</a>;'}).addTo(map);
 
-
     // var baseMaps = creationMaps();
     // console.log(baseMaps)
     // L.esri.Vector.vectorBasemapLayer('ArcGIS:Navigation', {
     //     apikey: "AAPK052bec1846714415aed2c85ddfa15f73KYexWiKoe0Au2nFQprFm_CWnafrYs4Y3MwTI3iqb-QBEwR808TRyXrudF4Za40V-" // Replace with your API key - https://developers.arcgis.com
     // }).addTo(map);
+
     var positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
           attribution: '©OpenStreetMap, ©CartoDB'
         }).addTo(map);
 
     var baseMaps = {"Basemap":positron}
-    var varMaps = {"satellite observation":image_layer,"water":water_layer,}
+    var varMaps = {"Satellite Observation":image_layer,"Water":water_layer,}
 
     L.control.layers(baseMaps,varMaps,{position: 'bottomleft'}).addTo(map);
 
@@ -95,41 +69,54 @@ $(function() {
        },
      });
      map.addControl(drawControl);
+     $("#load_data").click(function(){
+         let dataset = $('#dataset_id').val();
+         let end_date = $('#end_date').val();
+         let start_date = $('#start_date').val();
+         let terrain =  $("#terrain_correction_id").val();
+         let speckle =  $("#speckle_filter_id").val();
+         let cloud = $("#cloud_mask_id").val();
+ 
+         var request_obj={
+             'input_spatial':input_spatial,
+             'dataset' : dataset,
+             'start_date': start_date,
+             'end_date': end_date,
+             'terrain': terrain,
+             'speckle': speckle,
+             'cloud': cloud
+         }
+         console.log(request_obj);
+         $("#GeneralLoading").removeClass("hidden");
+         $.ajax({
+             type:"GET",
+             url:'get-image-layer/',
+             datatype:"JSON",
+             data:request_obj,
+             success: function(data){
+                 $("#GeneralLoading").addClass("hidden");
+                 console.log(data)
+                 water_layer.setUrl(data.water_url)
+                 image_layer.setUrl(data.image_url)
+                 // you need to make a pop up appear
+                 //1 create the pop in th html, your popup should have a button called close
+                 //2 make it appear $("#id").show();
+                
+             },
+             error: function(error){
+                 console.log(error)
+                 $("#GeneralLoading").addClass("hidden");
+                  // you need to make a pop up appear
+                 //1 create the pop in th html, your popup should have a button called close
+                 //2 make it appear $("#id").show();
+             }
+         })
+     })
      map.on(L.Draw.Event.CREATED, function (e) {
-        console.log('Draw Event Created');
-        drawnItems.addLayer(e.layer);
-        input_spatial = JSON.stringify(e.layer.toGeoJSON());
-        type_of_series = e.layerType;
-        let end_date = $('#end_date').val();
-        let start_date = $('#start_date').val();
-        let terrain =  $("#terrain_correction_id").val();
-        let speckle =  $("#speckle_filter_id").val();
-        let cloud = $("#cloud_mask_id").val();
-
-        var request_obj={
-            'input_spatial':input_spatial,
-            'start_date': start_date,
-            'end_date': end_date,
-            'terrain': terrain,
-            'speckle': speckle,
-            'cloud': cloud
-        }
-        console.log(request_obj);
-        $.ajax({
-            type:"GET",
-            url:'get-image-layer/',
-            datatype:"JSON",
-            data:request_obj,
-            success: function(data){
-                console.log(data)
-                water_layer.setUrl(data.water_url)
-                image_layer.setUrl(data.image_url)
-            },
-            error: function(error){
-                console.log(error)
-
-            }
-        })
+            // console.log('Draw Event Created');
+            drawnItems.addLayer(e.layer);
+            input_spatial = JSON.stringify(e.layer.toGeoJSON());
+            // type_of_series = e.layerType;
       });
 
     $.ajaxSetup({
