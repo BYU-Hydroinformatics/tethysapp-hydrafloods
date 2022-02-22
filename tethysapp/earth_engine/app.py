25a6b8e125a90a88d14d371b8e4f038f24c076f9
kaileymohamed <kaileymohamed@outlook.com>
2022-02-22 15:39:18 -0700

    app name change

--- tethysapp/earth_engine/app.py
+++ tethysapp/earth_engine/app.py
@@ -6,7 +6,7 @@ class EarthEngine(TethysAppBase):
     Tethys app class for Earth Engine.
     """
 
-    name = 'Earth Engine'
+    name = 'Water Mapping App'
     index = 'earth_engine:home'
     icon = 'earth_engine/images/earth-engine-logo.png'
     package = 'earth_engine'
