0707340d82b3d1ef56c32558e057b36d824a5859
romer8 <gio.busrom@gmail.com>
2022-03-28 19:38:41 -0600

    modules changes

--- tethysapp/earth_engine/params.py
+++ tethysapp/earth_engine/params.py
@@ -1,8 +1,8 @@
 import os
 # from .app import EarthEngine as app
-from . import app
+from .app import EarthEngine as EE
 
 dirname = os.path.dirname(__file__)
 
-service_account = app.EarthEngine.get_custom_setting('service_account')  # your google service account
-private_key = app.EarthEngine.get_custom_setting('path_to_json_key') # path to the json private key for the service account
+service_account = EE.get_custom_setting('service_account')  # your google service account
+private_key = EE.get_custom_setting('path_to_json_key') # path to the json private key for the service account
